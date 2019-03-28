d5d8ea78d0b6ba9b64b213e9ae5f97c0b6258b31
Aleksandr <ashvalev78@gmail.com>
2019-03-28 17:44:43 +0300

    some base is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,166 +1,65 @@
-var canvas;
-var ctx;
+const wrapper = document.querySelector('.wrapper');
+const popupBg = document.querySelector('.popup-bg');
+const closePopupBtn = document.querySelector('#close-popup');
+const popups = document.querySelectorAll('.popup');
 
-function init() {
-  canvas = initFullScreenCanvas('canvas');
-  ctx = canvas.getContext('2d');
-  repaint();
-
-  const data = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0],
-    [0, 0, 2, 1, 1, 2, 0],
-  ];
-
-}
-
-function initFullScreenCanvas(canvasId) {
-  const canvas = document.getElementById(canvasId);
-  resizeCanvas(canvas);
-  window.addEventListener('resize', function() {
-    resizeCanvas(canvas);
+function closePopups() {
+  popups.forEach((popup) => {
+    popup.classList.remove('visible');
+    wrapper.classList.remove('blur');
+    popupBg.classList.remove('visible');
   });
-  return canvas;
 }
 
-function resizeCanvas(canvas) { 
-  canvas.width = document.width || document.body.clientWidth;
-  canvas.height = document.height || document.body.clientHeight;
-  repaint();
-} 
-
-function repaint() {
-  if (!ctx) return;
-  var logo = new Image();
-  logo.src = '../images/content/bg.png';
-  var pattern = ctx.createPattern(logo, "repeat");
- ctx.fillStyle = pattern;
- ctx.lineWidth = 10;
- ctx.fillRect(0, 0, 800, 600); 
+function showPopup(popup) {
+  popup.classList.add('visible');
+  wrapper.classList.add('blur');
+  popupBg.classList.add('visible');
 }
 
-function reorient() {
-  let angle = window.orientation;
-  if (angle) {
-    const rot = -Math.PI*(angle/180);
-    ctx.translate(angle == -90 ? canvas.width : 0,
-      angle == 90 ? canvas.height : 0);
-    ctx.rotate(rot);
+closePopupBtn.addEventListener('click', closePopups);
+
+// радио-элемент для смены вида
+const view = document.querySelectorAll('.change-view__hidden');
+// список фото
+const photos = document.querySelector('.photos-collection__list');
+
+// изменение отображения списка фото в зависимости от предпочтений пользователя
+view[0].onchange = () => {
+  if (view[0].checked) {
+    photos.classList.add('grid-list');
+    photos.classList.remove('std-list');
+  } else {
+    photos.classList.remove('grid-list');
+    photos.classList.add('std-list');
   }
-} 
+};
+
+view[1].onchange = () => {
+  if (view[1].checked) {
+    photos.classList.remove('grid-list');
+    photos.classList.add('std-list');
+  } else {
+    photos.classList.add('grid-list');
+    photos.classList.remove('std-list');
+  }
+};
+
+// отображение попапа с выбранным фото
+
+// попап для изображения
+const imgPopup = document.querySelector('.img-popup');
+// коллекция изображений
+let images = document.querySelectorAll('.photos-collection__list-item');
 
 
+function takeCloserLook() {
+  images.forEach((image) => {
+    image.addEventListener('click', () => {
+      imgPopup.innerHTML = image.innerHTML;
+      showPopup(imgPopup);
+    });
+  });
+}
 
-
-
-// function init() {
-//   var canvas = document.getElementById("canvas");
-//   var ctx = canvas.getContext("2d");
- 
-//   // Background
-//   var gradient = ctx.createLinearGradient(0, 0, 0, 300);
-//   gradient.addColorStop(0, "#fffbb3");
-//   gradient.addColorStop(1, "#f6f6b2");
-//   ctx.fillStyle = gradient;
-//   ctx.fillRect(0, 0, canvas.width, canvas.height);
- 
-//   // Drawing curves
-//   ctx.strokeStyle = "#dad7ac";
-//   ctx.fillStyle = "#f6f6b2";
-//   ctx.beginPath();
-//   ctx.moveTo(50, 300);
-//   ctx.bezierCurveTo(450, -50, -150, -50, 250, 300);
-//   ctx.fill();
-//   ctx.beginPath();
-//   ctx.moveTo(50, 0);
-//   ctx.bezierCurveTo(450, 350, -150, 350, 250, 0);
-//   ctx.fill();
-//   // Border
-//   ctx.strokeStyle = "#989681";
-//   ctx.lineWidth = 2;
-//   ctx.strokeRect(1, 1, canvas.width - 2, canvas.height - 2);
- 
-//   // Grid and pieces (translate to center in on Canvas)
-//   var cellSize = 40;
-//   var gridWidth = cellSize*7;
-//   var gridHeight = cellSize*6; 
-
-//   var gridOffsetLeft = Math.floor((canvas.width - gridWidth)/2);
-//  var gridOffsetTop = Math.floor((canvas.height - gridHeight)/2);
-
-//  ctx.save();
-//  ctx.translate(gridOffsetLeft, gridOffsetTop);
-
-//  // Grid
-//  ctx.beginPath();
-
-//  // Drawing horizontal lines
-//  for (var i = 0; i < 8; i++) {
-//  ctx.moveTo(i*cellSize + 0.5, 0);
-//  ctx.lineTo(i*cellSize + 0.5, cellSize*6)
-//  }
-//  // Drawing vertical lines
-//  for (var j = 0; j < 7; j++) {
-//  ctx.moveTo(0, j*cellSize + 0.5);
-//  ctx.lineTo(cellSize*7, j*cellSize + 0.5);
-//  }
-//  // Stroking to show them on the screen
-//  ctx.lineWidth = 1;
-//  ctx.strokeStyle = "#989681";
-//  ctx.stroke();
-
-//  // Pieces
-//  var data = [
-//  [0, 0, 0, 0, 0, 0, 0],
-//  [0, 0, 0, 0, 0, 0, 0],
-//  [0, 0, 0, 0, 0, 0, 0],
-//  [0, 0, 0, 0, 0, 0, 0],
-//  [0, 0, 0, 2, 1, 0, 0],
-//  [0, 0, 2, 1, 1, 2, 0]
-//  ];
-//  ctx.strokeStyle = "#000";
-//  ctx.lineWidth = 3; 
-//  for (var i = 0; i < data.length; i++) {
-//   for (var j = 0; j < data[i].length; j++) {
-//   var value = data[i][j];
-//   if (!value)
-//   continue;
-//   // Determine the color of the token
-//   var color;
-//   switch (value) {
-//   case 1:
-//   color = "red"; 
-//   break;
-//   case 2:
-//   color = "green";
-//   break;
-//   }
-//   // Center of the token
-//   var x = (j + 0.5)*cellSize;
-//   var y = (i + 0.5)*cellSize;
- 
-//   // Token radius
-//   var radius = cellSize*0.4;
- 
-//   // Center of the gradient
-//   var gradientX = x + cellSize*0.1;
-//   var gradientY = y - cellSize*0.1; 
-//   var gradient = ctx.createRadialGradient(
-//     gradientX, gradientY, cellSize*0.1, // inner circle
-//     gradientX, gradientY, radius*1.2); // outer circle
-//     gradient.addColorStop(0, "yellow"); // the color of the
-//     gradient.addColorStop(1, color); // the color of the token
-//     ctx.fillStyle = gradient;
-//     ctx.beginPath();
-//     ctx.arc(x, y, radius, 0, 2*Math.PI, true);
-//     ctx.fill();
-//     }
-//     }
-//     // Restore the state of the context, since we've changed it
-//     ctx.restore()
-// } 
-
-document.body.onload = init;
+takeCloserLook();
